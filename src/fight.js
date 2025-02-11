44d5be728007c7c53522fa5f678eb86d98839c44
JoshiParSa <joshiparsa@gmail.com>
2025-02-10 19:49:02 -0500

    generate enemy

--- src/fight.js
+++ src/fight.js
@@ -1,13 +1,27 @@
 import { characters } from "./data/characters.js";
 
-function getCharacter() { 
+function getCharacter() {
   return JSON.parse(localStorage.getItem("selected_character"));
 }
 
-document.querySelector(".player").innerHTML = playerCard()
+document.querySelector(".player").innerHTML = playerCard();
+document.querySelector(".enemy").innerHTML = enemyCard();
 
 function playerCard() {
   const { name, ki, image } = getCharacter();
+  return `
+      <div class="card">
+        <img class="card-foreground" src="${image}" alt="${name}"></img>
+        <ul class="card-details">
+          <li>${name}</li>
+          <li>${ki}</li>
+        </ul>
+      </div>
+    `;
+}
+
+function enemyCard() {
+  const { name, ki, image } = characters[Math.floor(Math.random() * characters.length)];
   return `
       <div class="card">
         <img class="card-foreground" src="${image}" alt="${name}"></img>
