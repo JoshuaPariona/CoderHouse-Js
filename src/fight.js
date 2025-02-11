d6f383d505a37ad89f4d35f0af13ab77966794b5
JoshiParSa <joshiparsa@gmail.com>
2025-02-10 20:26:36 -0500

    refactor fight mechanics and UI elements for improved gameplay experience

--- src/fight.js
+++ src/fight.js
@@ -1,11 +1,17 @@
 import { characters } from "./data/characters.js";
 
+setDialogue("Tu turno, elige un movimiento");
+document.querySelector(".player").innerHTML = playerCard();
+document.querySelector(".enemy").innerHTML = enemyCard();
+localStorage.setItem("enemy", JSON.stringify(characters[Math.floor(Math.random() * characters.length)]));
+
 function getCharacter() {
   return JSON.parse(localStorage.getItem("selected_character"));
 }
 
-document.querySelector(".player").innerHTML = playerCard();
-document.querySelector(".enemy").innerHTML = enemyCard();
+function getEnemy() {
+  return JSON.parse(localStorage.getItem("enemy"));
+}
 
 function playerCard() {
   const { name, ki, image } = getCharacter();
@@ -21,7 +27,7 @@ function playerCard() {
 }
 
 function enemyCard() {
-  const { name, ki, image } = characters[Math.floor(Math.random() * characters.length)];
+  const { name, ki, image } = getEnemy();
   return `
       <div class="card">
         <img class="card-foreground" src="${image}" alt="${name}"></img>
@@ -31,4 +37,32 @@ function enemyCard() {
         </ul>
       </div>
     `;
+}
+
+function setDialogue(text) {
+  document.querySelector(".dialog").innerText = text;
+}
+
+document.querySelector(".attack").addEventListener("click", () => {
+  const player = getCharacter();
+  disableActions();
+})
+
+document.querySelector(".defend").addEventListener("click", () => {
+  const player = getCharacter();
+  disableActions();
+})
+
+document.querySelector(".ki").addEventListener("click", () => {
+  const player = getCharacter();
+  disableActions();
+})
+
+document.querySelector(".transform").addEventListener("click", () => {
+  const player = getCharacter();
+  disableActions();
+})
+
+function disableActions() {
+  document.querySelectorAll(".actions button").forEach(action => action.disabled = true);
 }
